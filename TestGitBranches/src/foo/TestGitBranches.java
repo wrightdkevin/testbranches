bb030ddcefd3564d648d96358007e79d728c03bf
kevin wright <wrightkevin2000@gmail.com>
2012-08-05 21:23:40 +0100

    First Cut

---
+++ TestGitBranches/src/foo/TestGitBranches.java
@@ -0,0 +1,15 @@
+package foo;
+
+public class TestGitBranches {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world"); 
+		
+
+	}
+
+}
